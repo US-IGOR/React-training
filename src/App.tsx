b6062be9e5169b70ab85e256b3bd7128a44eafaf
Igor.Uss <iigor.uss@gmail.com>
2021-10-01 19:59:20 +0300

    1.components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            This is APP component
+            <Rating />
+            <Accordion/>
+        </div>
+    );
 }
+
+
+function Rating() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    )
+}
+
+function Accordion () {
+    return <div>
+        <h3>Menu</h3>
+        <ul>1</ul>
+        <ul>2</ul>
+        <ul>3</ul>
+    </div>
+}
+
+
+
 
 export default App;
